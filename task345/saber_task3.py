77490692412d1d6f609a62f0233564496ddc2380
elifehacker <steven_wang_pei@hotmail.com>
2021-08-06 09:44:05 +1000

    minor changes before adapting a more saber look

--- task345/saber_task3.py
+++ task345/saber_task3.py
@@ -323,4 +323,5 @@ if __name__ == '__main__':
 https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
 https://stackoverflow.com/questions/9581692/recommended-date-format-for-rest-get-api
 https://towardsdatascience.com/free-stock-data-for-python-using-yahoo-finance-api-9dafd96cad2e
+https://www.chartjs.org/samples/2.6.0/
 '''
